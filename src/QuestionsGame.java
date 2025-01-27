869f9b07506bc1836b32b6fed6fc3305d9432178
LaurenMurillo <murillolauren62@gmail.com>
2025-01-27 14:15:15 -0600

    QuestionNode class

--- src/QuestionsGame.java
+++ src/QuestionsGame.java
@@ -3,10 +3,27 @@
 // You should delete this comment and replace it with your class
 // header comment.
 
-public class QuestionsGame {
+public class QuestionsGame 
+{
     // Your code here
 
-    private static class QuestionNode {
-        // Your code here
+    private static class QuestionNode 
+    {
+    	public final boolean data;   // data stored at this node
+        public QuestionNode left;    // reference to left subtree
+        public QuestionNode right;   // reference to right subtree
+        
+        public QuestionNode(int data) 
+        {
+            this(data, null, null);
+        }
+                    
+        // Constructs a branch node with the given data and links.
+        public QuestionNode(int data, QuestionNode left, QuestionNode right) 
+        {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
     }
 }
