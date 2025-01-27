83804944764229c0f06b486d9595ab1af927a1e9
akanduri15 <abhinavkanduri@icloud.com>
2025-01-27 14:29:33 -0600

    Updated QuestionsGame num1

--- src/QuestionsGame.java
+++ src/QuestionsGame.java
@@ -2,28 +2,42 @@
 //
 // You should delete this comment and replace it with your class
 // header comment.
-
+import java.util.Scanner;
 public class QuestionsGame 
 {
     // Your code here
 
     private static class QuestionNode 
     {
-    	public final boolean data;   // data stored at this node
+    	public final String data;   // data stored at this node
         public QuestionNode left;    // reference to left subtree
         public QuestionNode right;   // reference to right subtree
         
-        public QuestionNode(int data) 
+        public QuestionNode(String data) 
         {
             this(data, null, null);
         }
                     
         // Constructs a branch node with the given data and links.
-        public QuestionNode(int data, QuestionNode left, QuestionNode right) 
+        public QuestionNode(String data, QuestionNode left, QuestionNode right) 
         {
             this.data = data;
             this.left = left;
             this.right = right;
         }
+    }
+    
+    public QuestionsGame(String object)
+    {
+    	QuestionsGame questionsGame = new QuestionsGame(object);
+    }
+    public QuestionsGame(Scanner input)
+    {
+    	QuestionsGame questionsGame = new QuestionsGame(input);
+    			
+    }
+    public void saveQuestions(PrintStream output)
+    {
+    	
     }
 }
